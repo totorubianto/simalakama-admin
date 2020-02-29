0ad77625d6c5f77993137aa6c075b440ee589cc2
Toto Rubianto <toto.rubianto.17@gmail.com>
2020-02-29 18:12:49 +0700

    Update : footer and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes } from "./router/router";
 import "./App.css";
-import Dashboard from "./components/dashboard/dashboard";
 const App: React.FC = () => {
   return (
     <div className="App">
---
+++ src/components/global/footer/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const footer: React.FC<Props> = () => {
+  return (
+    <div>
+      <div>sdfs</div>
+    </div>
+  );
+};
+
+export default footer;
